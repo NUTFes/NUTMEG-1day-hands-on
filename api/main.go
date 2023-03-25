536cc2cd05b4ddd1c6c661536c0a370c306a080e
ryuseifujisaki <fujiryu1122@gmail.com>
2023-03-26 01:33:58 +0900

    DB接続完了

--- api/main.go
+++ api/main.go
@@ -1,13 +1,35 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
+var DB *gorm.DB
+
+func sqlConnect() *gorm.DB {
+	USER := os.Getenv("MYSQL_USER")
+	PASS := os.Getenv("MYSQL_PASSWORD")
+	PROTOCOL := "tcp(db:3306)"
+	DBNAME := os.Getenv("MYSQL_DATABASE")
+	dsn := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?parseTime=true&charset=utf8&loc=Local"
+	DB, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println("db connected")
+	return DB
+}
+
 func main() {
+
 	e := echo.New()
+	DB = sqlConnect()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
