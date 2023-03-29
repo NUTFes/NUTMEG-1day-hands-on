df4616cc3cb4dbbf398dccd252ed3809f4c6a9cc
imaimai17468 <tosiki_801_life@outlook.jp>
2023-03-30 00:36:38 +0900

    [feat] add components in common

--- view/src/components/common/index.ts
+++ view/src/components/common/index.ts
@@ -1,1 +1,8 @@
 export { Button } from "./Button";
+export { Card } from "./Card";
+export { TaskCard } from "./TaskCard";
+export { TaskListCard } from "./TaskListCard";
+export { Modal } from "./Modal";
+export { TaskAddModal } from "./TaskAddModal";
+export { TaskEditModal } from "./TaskEditModal";
+export { TaskDeleteModal } from "./TaskDeleteModal";
