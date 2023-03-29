ff85c744cf7f1e06a4a8ad4df740ac90518efea5
imaimai17468 <tosiki_801_life@outlook.jp>
2023-03-30 00:39:21 +0900

    [feat] app.tsxにheadとmainlayoutを追加

--- view/src/pages/_app.tsx
+++ view/src/pages/_app.tsx
@@ -1,6 +1,19 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { MainLayout } from "@/components/layout";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>NUTMEG ToDo</title>
+        <meta name="description" content="ToDoアプリ" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </>
+  );
 }
