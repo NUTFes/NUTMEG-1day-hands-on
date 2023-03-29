5c35afc12e30af61dbdc81f6131882ec245f1caa
imaimai17468 <tosiki_801_life@outlook.jp>
2023-03-30 00:39:07 +0900

    [feat] add header in screen

---
+++ view/src/components/screen/Header/Header.tsx
@@ -0,0 +1,9 @@
+export const Header: React.FC = () => {
+  return (
+    <div className="w-full bg-primary text-secondary h-16 flex justify-center items-center shadow-xl">
+      <h1 className="text-2xl font-bold">NUTMEG ToDo</h1>
+    </div>
+  );
+};
+
+export default Header;
